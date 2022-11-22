c79e0b21459e2648b37cb8a90981c6d6cb18961f
Kim <kim.shakhov@gmail.com>
2022-11-22 21:13:20 +0100

    refactor
    code cleanup, refactoring
    split project into files

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+import test_boards
+import solver
+
+if __name__ == '__main__':
+    matrix_to_solve = test_boards.T_1
+    solver.print_matrix(matrix_to_solve)
+    t0 = time.time()
+    is_solvable = solver.start_matrix_solve(matrix_to_solve)
+    t1 = time.time()
+    print()
+    solver.print_matrix(matrix_to_solve)
+    print(is_solvable)
+    print(t1 - t0)
---
+++ solver.py
@@ -0,0 +1,66 @@
+def print_matrix(matrix):
+    countdown = 0
+    countright = 0
+    print()
+    print()
+    for r in matrix:
+        for c in r:
+            print(c, end=" ")
+            countright += 1
+            if countright % 3 == 0: print('|', end=" ")
+        print()
+        countdown += 1
+        if countdown % 3 == 0: print("----- | ----- | -----")
+
+
+def check_new_entry(matrix, c, r):
+    new = matrix[r][c]
+    for i in range(9):
+        if matrix[i][c] == new and i != r:
+            return False
+    for j in range(9):
+        if matrix[r][j] == new and j != c:
+            return False
+    for i in range(c - c % 3, c - c % 3 + 3):
+        for j in range(r - r % 3, r - r % 3 + 3):
+            if i == c and j == r: continue
+            if matrix[j][i] == new:
+                return False
+    return True
+
+
+def solve_matrix(matrix, c, r):
+    if matrix[r][c] == 9:
+        matrix[r][c] = 0
+        return False
+
+    matrix[r][c] += 1
+    if check_new_entry(matrix, c, r):
+        (i, j) = find_empty(matrix)
+        if (i, j) == (9, 9) or solve_matrix(matrix, j, i):
+            return True
+        else:
+            return solve_matrix(matrix, c, r)
+    else:
+        return solve_matrix(matrix, c, r)
+
+
+def find_empty(matrix):
+    i, j = 0, 0
+    while i < 9 and j < 9:
+        if matrix[i][j] == 0:
+            return (i, j)
+        if j != 8:
+            j, i = j + 1, i
+        elif i != 8:
+            j, i = 0, i + 1
+        else:
+            return (9, 9)
+
+
+def start_matrix_solve(matrix):
+    (i, j) = find_empty(matrix)
+    if (i, j) == (9, 9):
+        return True
+    else:
+        return solve_matrix(matrix, j, i)
