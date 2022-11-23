810feea8847a9087613f826a8a7b9a3bed83f5b5
Kim <kim.shakhov@gmail.com>
2022-11-23 23:10:21 +0100

    no multiprocessing
    only 1 process left for now

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
-import multiprocessing
 
 import solve_GUI
 import test_boards
-import solver
 
 
-class Process(multiprocessing.Process):
-    def __init__(self, proc_id, matrix):
-        super(Process, self).__init__()
-        self.id = proc_id
-        self.matrix = matrix
-
-    def run(self):
-        if self.id == 0:
-            solve_GUI.run_GUI(self.matrix)
-        if self.id == 1:
-            print("welcome To Kim's sudoku solver")
-            solver.print_matrix(self.matrix)
-            is_solvable = solver.start_matrix_solve(self.matrix)
-            solver.print_matrix(self.matrix)
-            print(is_solvable[1])
 
 
 if __name__ == '__main__':
     matrix_to_solve = test_boards.T_1
+    solve_GUI.run_GUI(matrix_to_solve)
 
-    p0 = Process(0, matrix_to_solve)
-    p0.start()
-    p1 = Process(1, matrix_to_solve)
-    p1.start()
-    p0.join()
-    p1.join()
